04d22f796035143f5d15b4844eeb32131b0ca1c9
KyTama <prtama95@gmail.com>
2019-10-23 23:31:28 +0700

    Add view & Hero List

--- app/src/main/java/com/dicodeky/myrecyclerview/MainActivity.java
+++ app/src/main/java/com/dicodeky/myrecyclerview/MainActivity.java
@@ -1,14 +1,66 @@
 package com.dicodeky.myrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.dicodeky.myrecyclerview.adapter.ListHeroAdapter;
+import com.dicodeky.myrecyclerview.model.Hero;
+import com.dicodeky.myrecyclerview.model.HeroesData;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView rvHeroes;
+    private ArrayList<Hero> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvHeroes = findViewById(R.id.rv_heroes);
+        rvHeroes.setHasFixedSize(true);
+
+        list.addAll(HeroesData.getListData());
+        showRecyclerList();
+
+    }
+
+    private void showRecyclerList(){
+        rvHeroes.setLayoutManager(new LinearLayoutManager(this));
+        ListHeroAdapter listHeroAdapter = new ListHeroAdapter(list);
+        rvHeroes.setAdapter(listHeroAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        setMode(item.getItemId());
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void setMode(int selectedMode) {
+        switch (selectedMode) {
+            case R.id.action_list:
+                break;
+
+            case R.id.action_grid:
+                break;
+
+            case R.id.action_cardview:
+                break;
+
+
+        }
     }
 }
